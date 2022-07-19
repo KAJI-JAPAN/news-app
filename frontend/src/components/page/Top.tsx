8f0ade38cacfdef9375ac7ddb29b753d3fbc1e9e
KAJI-JAPAN <kurowassan1206@gmail.com>
2022-07-19 15:46:44 +0900

    apiキーを環境変数に追加

--- frontend/src/components/page/Top.tsx
+++ frontend/src/components/page/Top.tsx
@@ -1,21 +1,42 @@
-import { useEffect } from "react"
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, Flex, Image } from "@chakra-ui/react"
+import { FC, memo, useEffect } from "react"
 import { useGetTopNews } from "../../hooks/useGetTopNews"
 import { BeforeLoginHeader } from "../organisms/headers/BeforeLoginHeader"
 
-export const Top = () => {
+export const Top: FC = memo(() => {
   const { getTopNews, topNews } = useGetTopNews()
 
-  useEffect(() => getTopNews(), [getTopNews])
+  useEffect(() =>  getTopNews(), [getTopNews])
 
   return (
     <>
       <BeforeLoginHeader />
       { topNews.map((news) => (
-        <div>
-          {news.title}
-        </div>
+          <Accordion allowToggle>
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box flex='1' textAlign='left'>
+                      <h2>{news.title}</h2>
+                  </Box>
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <Flex>
+                {news.description}
+                <Image 
+                  src={news.urlToImage}
+                  alt={news.title}
+                  boxSize="150px"
+                  objectFit='cover'
+                  mx='auto'
+                />
+                </Flex>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
         ))
-      }
+      } 
     </>
   )
-}
+})
--- frontend/src/hooks/useGetTopNews.ts
+++ frontend/src/hooks/useGetTopNews.ts
@@ -1,15 +1,15 @@
 import { AllNews, Article } from './../types/AllNews';
 import  axios  from 'axios';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export const useGetTopNews = () => {
   const [ topNews, setTopNews] = useState<Article[]>([])
 
-  const getTopNews = () => {
-    axios.get<AllNews>('https://newsapi.org/v2/top-headlines?country=us&apiKey=c1ab40bdb9f94aefb3d63dcc6a952a7d')
+  const getTopNews = useCallback(() => {
+    axios.get<AllNews>(`https://newsapi.org/v2/top-headlines?country=jp&apiKey=${process.env.REACT_APP_NEWS_API}`)
     .then((res) => {
       setTopNews(res.data.articles)
     })
-  }
+  }, [])
   return { getTopNews, topNews }
 }
