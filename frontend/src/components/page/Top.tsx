49c7d505e46303bc30b787e8d29fd22ec28f3e6a
KAJI-JAPAN <kurowassan1206@gmail.com>
2022-07-23 11:24:49 +0900

    タイトル追加

--- frontend/src/components/page/Top.tsx
+++ frontend/src/components/page/Top.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react"
+import { Box, Text } from "@chakra-ui/react"
 import { FC, memo, useEffect } from "react"
 import { useGetTopNews } from "../../hooks/useGetTopNews"
 import { BeforeLoginHeader } from "../organisms/headers/BeforeLoginHeader"
@@ -13,11 +13,13 @@ export const Top: FC = memo(() => {
     <>
       <BeforeLoginHeader />
       <Box maxW="800px" mx='auto' py={5}>
+      <Text fontSize='3xl' pb={5}>Top News</Text>
       { topNews.map((news) => (
         <TopNews
           title={news.title}
           image={news.urlToImage}
           description={news.description}
+          // url={news.url}
         />
       ))
       }
