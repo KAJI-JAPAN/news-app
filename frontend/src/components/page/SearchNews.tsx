471a5bef0bb770e6baf12f983e76f915c0ac65bc
KAJI-JAPAN <kurowassan1206@gmail.com>
2022-07-25 21:04:34 +0900

    searchResultページを追加

---
+++ frontend/src/components/page/SearchNews.tsx
@@ -0,0 +1,7 @@
+export const SearchNews = () => {
+  return (
+    <>
+      <p>検索結果を表示</p>
+    </>
+  )
+}
--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,11 +1,13 @@
 import { FC, memo } from "react";
 import { Route, Routes } from "react-router-dom";
+import { SearchNews } from "../components/page/SearchNews";
 import { Top } from "../components/page/Top";
 
 export const Router: FC= memo(() => {
   return (
     <Routes>
       <Route path="/top" element={<Top />} />
+      <Route path="/searchResult" element={<SearchNews />} />
     </Routes>
   )
 })
